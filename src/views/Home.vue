cc3468f2e1290fc93ea29c824cf38f50057311dc
felixand <felix.andrean17@gmail.com>
2019-07-15 06:02:49 +0700

    first init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="home">
+  	<div class="banner-full">
+	  	<div class="main-title">
+	  		<h3>Star Wars</h3>
+	  	</div>
+  		<vue-particles 
+  			color="#ffffff"
+  			shapeType="star"
+  			:particleSize="2"
+  			:lineLinked="false"
+  			:particleOpacity="0.7"
+  			hoverMode="repulse"
+  			class="particle-js"
+  		>
+  		</vue-particles>
+  	</div>
   </div>
 </template>
 
@@ -16,3 +29,29 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+	#home{
+		.banner-full{
+			background: #010314;
+			height: 100vh;
+			position: relative;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			.main-title{
+				color: #fff;
+				z-index: 999;
+			}
+
+			.particle-js{
+				position: absolute;
+		    top: 0;
+		    left: 0;
+		    width: 100%;
+		    height: 100%;
+			}
+		}
+	}
+</style>
